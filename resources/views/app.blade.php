b89731f8c83cec2e12b27c27dc3c5e7bd0c6b772
Atilio3108 <salassistema03@gmail.com>
2026-01-28 14:15:13 -0300

    feat: configurar rutas de laravel, cors y vista principal para integración con Vue

---
+++ resources/views/app.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" href="{{ asset('favicon.ico') }}" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Salas</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-submenu@3.0.1/dist/css/bootstrap-submenu.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" crossorigin href="{{ asset('assets/index-DsxwMagj.css') }}">
+  </head>
+  <body>
+
+    <div id="app"></div>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-submenu@3.0.1/dist/js/bootstrap-submenu.min.js"></script>
+
+    <script type="module" crossorigin src="{{ asset('assets/index-DR3X9Lp0.js') }}"></script>
+
+  </body>
+</html>
