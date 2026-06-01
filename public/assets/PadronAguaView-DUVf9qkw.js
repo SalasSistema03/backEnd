650a5627b6f1d32abc15a3ceb603cf43aa477cd6
Atilio3108 <salassistema03@gmail.com>
2026-06-01 13:50:18 -0300

    arreglo subida de pdfs

---
+++ public/assets/PadronAguaView-DUVf9qkw.js
@@ -0,0 +1,1 @@
+import{c as e,b as a,_ as t,F as o,j as r}from"./index-CRC9mrUL.js";import{_ as s}from"./PadronImpuesto-BUPEtdQ6.js";import"./useDateFormatter-Ca4_OelW.js";import"./tgiApi-eYrbmdnW.js";import"./BaseModal-DdvLIAt0.js";const m="agua",d={__name:"PadronAguaView",setup(_){return(c,n)=>(r(),e(o,null,[a(t),a(s,{impuesto:m})],64))}};export{d as default};
