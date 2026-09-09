e0fe657b4f90e35100df7a9874e7f030bb888843
Atilio3108 <salassistema03@gmail.com>
2026-09-09 07:58:11 -0300

    M

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,7 +30,7 @@
     <!-- Custom CSS -->
     <!-- App -->
     <title>Salas</title>
-    <script type="module" crossorigin src="/assets/index-BY8dlXrK.js"></script>
+    <script type="module" crossorigin src="/assets/index-CVowfWkD.js"></script>
     <link rel="stylesheet" crossorigin href="/assets/index-CY34SmdX.css">
   </head>
   <body>
